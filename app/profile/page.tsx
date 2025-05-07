52e4a5cddc080a2485bbe85aefc5bccee0fd2f02
mateenikhtiyar <mateenikhtiyar8@gmail.com>
2025-05-07 22:53:16 +0500

    Updating

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -379,9 +379,9 @@ export default function ProfilePage() {
                     />
                   )}
                 </button>
-                <div className="absolute -right-1 -top-1 flex h-4 w-4 items-center justify-center rounded-full bg-red-500 text-xs text-white">
+                {/* <div className="absolute -right-1 -top-1 flex h-4 w-4 items-center justify-center rounded-full bg-red-500 text-xs text-white">
                   1
-                </div>
+                </div> */}
               </div>
               <ChevronDown className="ml-1 h-4 w-4 text-gray-500" />
             </div>
