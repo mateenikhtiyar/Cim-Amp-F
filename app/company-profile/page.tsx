5c7a2f0a4d07dfe752aca68ae9332b810dc57ee4
mateenikhtiyar <mateenikhtiyar8@gmail.com>
2025-05-09 10:51:35 +0500

    Updating

--- app/company-profile/page.tsx
+++ app/company-profile/page.tsx
@@ -42,8 +42,6 @@ const COMPANY_TYPES = [
   "Single Acquisition Search",
   "Strategic Operating Company",
   "Buy Side Mandate",
-  "Strategic Operating Company",
-  "Buy Side Mandate",
 ]
 
 const CAPITAL_ENTITIES = ["Fund", "Holding Company", "SPV", "Direct Investment"]
